28a47b16db2354bfea6d6f7705d11a4deec5aae0
JhimmyOfficial <72657872+OfficialJhimmy@users.noreply.github.com>
2022-06-09 21:25:59 +0100

    Update packages.jsx

--- src/pages/dashboard/packages/packages.jsx
+++ src/pages/dashboard/packages/packages.jsx
@@ -66,29 +66,13 @@ const Packages = () => {
                 <h5>Packages</h5>
                 <div className="price-list">
                   <div className="tier">
-                    <h6 className="title">SMM Starter Pack </h6>
-                    <h4>
-                      #20,000<span>/month</span>
-                    </h4>
-                    <p>(#220,000/year)</p>
-                    <ul>
-                      <li>Management of 2 Social Media Accounts</li>
-                      <li>6 creative designs monthly</li>
-                      <li>Monthly Custom Reports</li>
-                      <li>Performance evaluation and recommendation</li>
-                    </ul>
-                    <h6>
-                      This pack includes creative designs, update, situational
-                      report and recommendation
-                    </h6>
-                  </div>
-                  <div className="tier">
-                    <h6 className="title">SMM Brand Pack</h6>
+                    <h6 className="title">Pepper Jollof </h6>
                     <h4>
                       #50,000<span>/month</span>
                     </h4>
                     <p>(#550,000/year)</p>
                     <ul>
+                      <li>Social media strategy</li>
                       <li>Management of 4 Social Media Accounts</li>
                       <li>12 creative designs monthly</li>
                       <li>Monthly Custom Reports</li>
@@ -99,61 +83,59 @@ const Packages = () => {
                     </ul>
                     <h6>
                       This pack includes creative designs, update, situational
-                      reports, recommendation and promotion.{" "}
+                      report and recommendation
                     </h6>
                   </div>
                   <div className="tier">
-                    <h6 className="title">SMM Professional Pack</h6>
+                    <h6 className="title">Sunday Jollof</h6>
                     <h4>
-                      #150,000<span>/month</span>
+                      #170,000<span>/month</span>
                     </h4>
-                    <p>(#1.7 million/year)</p>
+                    <p>(#1.9 million/year)</p>
                     <ul>
+                      <li>Social media strategy</li>
                       <li>Management of 4 Social Media Accounts</li>
                       <li>20 creative designs monthly</li>
                       <li>Monthly Custom Reports</li>
                       <li>Performance evaluation and recommendation</li>
+                      <li>1 week PPC (Pay Per Click) ads campaign</li>
                       <li>6 promoted on Facebook or Instagram</li>
                       <li>A recommendation call once a month</li>
                       <li>Audience Targeting</li>
-                      <li>DM Customer Care</li>
-                      <li>Comment Moderation</li>
                       <li>Content Calendar</li>
+                      <li>Account Manager Customer care</li>
                     </ul>
                     <h6>
                       This pack includes creative designs, update, situational
-                      reports, recommendation, promotion and an assigned account
-                      manager.{" "}
+                      reports, recommendation, promotion and an assigned account manager.
                     </h6>
                   </div>
                   <div className="tier">
-                    <h6 className="title">BRAND XTRA </h6>
+                    <h6 className="title">Local Jollof</h6>
                     <h4>
-                      #200,000<span>/month</span>
+                      #250,000<span>/month</span>
                     </h4>
-                    <p>(#2.2 million/year)</p>
+                    <p>(#2.8 million/year)</p>
                     <ul>
+                      <li>Social media strategy</li>
                       <li>Management of 4 Social Media Accounts</li>
                       <li>12 creative designs monthly</li>
                       <li>Monthly Custom Reports</li>
                       <li>Performance evaluation and recommendation</li>
-                      <li>6 promoted on Facebook or Instagram</li>
+                      <li>2 weeks PPC (Pay Per Click) ads campaign</li>
+                      <li>6 Facebook/Instagram Ad campaign</li>
                       <li>A recommendation call once a month</li>
                       <li>Audience Targeting</li>
-                      <li>Social Media Strategy </li>
-                      <li>DM Customer Care</li>
                       <li>250,000 CPM on publishers websites</li>
-                      <li>Comment Moderation</li>
                       <li>Content Calendar</li>
+                      <li>Account Manager Customer care</li>
                     </ul>
                     <h6>
-                      This pack includes creative designs, update, situational
-                      reports, recommendation, promotions, an assigned account
-                      manager and advert on blogs and websites.
+                    This pack includes creative designs, update, situational reports, recommendation, promotions, an assigned account manager and advert on blogs and websites.
                     </h6>
                   </div>
                   <div className="tier">
-                    <h6 className="title">PROFESSIONAL XTRA </h6>
+                    <h6 className="title">Owambe Jollof </h6>
                     <h4>
                       #500,000<span>/month</span>
                     </h4>
@@ -162,28 +144,50 @@ const Packages = () => {
                       <li>Management of 3 Social Media Accounts</li>
                       <li>20 creative designs monthly</li>
                       <li>Monthly Custom Reports</li>
+                      <li>1 Custom Video Creation</li>
                       <li>Performance evaluation and recommendation</li>
-                      <li>10 promoted on Facebook or Instagram</li>
-                      <li>A recommendation call once a month</li>
+                      <li>10 Facebook/Instagram campaign Ads</li>
                       <li>Lookalike Audience Targeting</li>
                       <li>Social Media Strategy </li>
-                      <li>DM Customer Care</li>
                       <li>500,000 CPM on publishers websites</li>
-                      <li>1 Custom Video Creation</li>
                       <li>1000 real followers </li>
-                      <li>Comment Moderation</li>
                       <li>Content Calendar</li>
+                      <li>Account Manager Customer care</li>
                     </ul>
                     <h6>
-                      This pack includes creative designs, update, situational
-                      reports, recommendation, promotions, an assigned account
-                      manager and advert on blogs and websites.{" "}
+                    This pack includes creative designs, update, situational reports, recommendation, promotions, an assigned account manager and advert on blogs and websites.
                     </h6>
                   </div>
                   <div className="tier">
-                    <h6 className="title">CHERRY COMBO </h6>
+                    <h6 className="title">Jollof Jollof </h6>
                     <h4>
-                      #1 million<span>/month</span>
+                      #1.5 million<span>/month</span>
+                    </h4>
+                    <p>(#16 million/year)</p>
+                    <ul>
+                      <li>Management of 4 Social Media Accounts</li>
+                      <li>Unlimited creative designs monthly</li>
+                      <li>Monthly Custom Reports</li>
+                      <li>Performance evaluation and recommendation</li>
+                      <li>20 promoted on Facebook or Instagram</li>
+                      <li>1 month PPC (Pay Per Click) ads campaign</li>
+                      <li>Lookalike Audience Targeting</li>
+                      <li>Social Media Strategy </li>
+                      <li>Over 750,000 CPM on publishers websites</li>
+                      <li>2 Custom Video Creation</li>
+                      <li>1500 real followers </li>
+                      <li>Content Calendar</li>
+                      <li>Email marketing</li>
+                      <li>Account Manager Customer care</li>
+                    </ul>
+                    <h6>
+                    This pack includes creative designs, update, situational reports, recommendation, promotions, an assigned account manager and advert on blogs and websites.
+                    </h6>
+                  </div>
+                  <div className="tier">
+                    <h6 className="title">Pantry Jollof</h6>
+                    <h4>
+                      #2 million<span>/month</span>
                     </h4>
                     <p>(#11 million/year)</p>
                     <ul>
@@ -192,20 +196,19 @@ const Packages = () => {
                       <li>Monthly Custom Reports</li>
                       <li>Performance evaluation and recommendation</li>
                       <li>20 promoted on Facebook or Instagram</li>
-                      <li>Recommendation calls</li>
+                      <li>1 month PPC (Pay Per Click) ads campaign</li>
                       <li>Lookalike Audience Targeting</li>
                       <li>Social Media Strategy </li>
-                      <li>DM Customer Care</li>
                       <li>Over 1 million CPM on publishers websites</li>
-                      <li>2 Custom Video Creation</li>
-                      <li>1500 real followers </li>
+                      <li>3 Custom Video Creation</li>
+                      <li>2000 real followers </li>
                       <li>Comment Moderation</li>
                       <li>Content Calendar</li>
+                      <li>Email marketing</li>
+                      <li>Account Manager Customer care</li>
                     </ul>
                     <h6>
-                      This pack includes creative designs, update, situational
-                      reports, recommendation, promotions, an assigned account
-                      manager and advert on blogs and websites.
+                    This pack includes creative designs, update, situational reports, recommendation, promotions, an assigned account manager and advert on blogs and websites.
                     </h6>
                   </div>
                 </div>
